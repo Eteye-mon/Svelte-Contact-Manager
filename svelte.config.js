40617f2a74ad6b3e97d54cdff6ea670e1be6dabf
monday.eteye <emandymind@gmail.com>
2025-10-03 11:52:06 +0100

    updated packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ out: "build" }),
   },
   vitePlugin: {
     inspector: true,
